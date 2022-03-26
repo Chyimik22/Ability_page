408bc6cc01302b85dc09c3d0d292c89806deffb4
Chumik22 <rudiuk.ivan.jan@gmail.com>
2022-03-26 21:40:51 +0100

    A tam

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./styles.css";
 import settings from "./settings.js";
 import app from "./app";
+//if (module.hot) { module.hot.accept(function () { location.reload(); }); }
+docReady(function() {
+    // DOM is loaded and ready for manipulation here
+    app(settings);
 
-app(settings);
+});
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
