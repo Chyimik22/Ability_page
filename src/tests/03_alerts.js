a8eaa5773852e92c0873ee603568de4f2045f5e4
Chumik <rudiuk.ivan.jan@gmail.com>
2022-03-28 02:12:14 +0200

    Added buttons Added new buttons but trouble with new lines in between them

--- src/tests/03_alerts.js
+++ src/tests/03_alerts.js
@@ -2,8 +2,9 @@ export default function () {
   function alert1() {
     alert("Avoide this page");
   }
-  function yesORno() {
-    if (confirm("You still want to be here?")) {
+  function yesOrNo() {
+    let question_conf = confirm("You still want to be here?");
+    if (question_conf) {
       alert("I am so proud of you");
     } else {
       alert("You better to get out");
@@ -20,13 +21,35 @@ export default function () {
       alert("You have canseled my request and her what i got insted " + name);
     }
   }
+  const nline = document.createElement("br");
 
   let butt_alert1 = document.createElement("button");
-  butt_alert1.setAttribute("id", "alert1");
+  butt_alert1.setAttribute("id", "butt_alert1");
   butt_alert1.appendChild(document.createTextNode("Popup alert"));
-  document.getElementById("app").appendChild(butt_alert1); //it's need to apear in app
-  document.getElementById("alert1").onclick = function () {
+  document.getElementById("app").appendChild(butt_alert1);
+  document.getElementById("butt_alert1").onclick = function () {
     alert1();
   };
-  //return "";
+
+  document.getElementById("app").appendChild(nline.cloneNode(true));
+  //document.getElementById("app").innerHTML = "<br>";
+  //Do not work as expected, or it is clearing HTML before
+
+  let butt_quest = document.createElement("button");
+  butt_quest.setAttribute("id", "butt_quest");
+  butt_quest.appendChild(document.createTextNode("Question"));
+  document.getElementById("app").appendChild(butt_quest);
+  document.getElementById("butt_quest").onclick = function () {
+    question();
+  };
+  //document.getElementById("butt_quest").appendChild(nline);
+  document.getElementById("app").appendChild(nline.cloneNode(true));
+
+  let butt_yesOrNo = document.createElement("button");
+  butt_yesOrNo.setAttribute("id", "butt_yesOrNo");
+  butt_yesOrNo.appendChild(document.createTextNode("Yes Or No"));
+  document.getElementById("app").appendChild(butt_yesOrNo);
+  document.getElementById("butt_yesOrNo").onclick = function () {
+    yesOrNo();
+  };
 }
