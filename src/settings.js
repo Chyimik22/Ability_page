f8c73b285ede996483ca2b09d247c22f78b54cf2
Chumik22 <rudiuk.ivan.jan@gmail.com>
2022-04-03 21:23:20 +0200

    Optimalisation works

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,23 @@
 export default {
-  menuItems: [
-    { name: "01_var_const", menuName: "var_const" },
-    { name: "02_tables", menuName: "tables" },
-    { name: "03_alerts", menuName: "alerts" }
-  ]
+    tests: [
+        {
+            name: '01_var_const',
+            import: import('./tests/01_var_const'),
+            menuName: "var_const"
+        },
+
+        {
+            name: '02_tables',
+            import: import('./tests/02_tables'),
+            menuName: "tables"
+        },
+        {
+            name: '03_alerts',
+            import: import('./tests/03_alerts'),
+            menuName: "alerts"
+        }
+    ]
 };
+
+
+
