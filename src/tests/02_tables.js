95f47a272eed1d851d26ed35ed3581d69668d16e
Chumik22 <rudiuk.ivan.jan@gmail.com>
2022-04-12 12:38:18 +0200

    Code optimisation

--- src/tests/02_tables.js
+++ src/tests/02_tables.js
@@ -1,79 +1,37 @@
-import {createButton, test} from "../services/htmlElements.js";
+import {createButton} from "../services/htmlElements.js";
 
 export default function () {
-    document.getElementById("app").innerHTML = "To see more check console<br>";
+    document.getElementById("app").innerHTML = "To see more check console <br>";
     const result = document.createElement("div");
     result.setAttribute("id", "result");
     document.getElementById("app").append(result);
     let table = [];
+    let input = document.createElement("input");
+    input.setAttribute("id", "input",
+        "type", "text",
+        "placeholder", "Enter data");
+    document.getElementById("app").append(input);
 
-    function createInputField() {
-        let input = document.createElement("input");
-        input.setAttribute("id", "input",
-            "type", "text",
-            "name", "input",
-            "placeholder", "Your data");
-        document.getElementById("app").appendChild(input);
-    }
+    createButton(function () {
+        if (input.value !== "") {
+            table.push(input.value);
+            console.log(table);
+            input.value = "";
+        }
+    }, "addButton", "Add");
 
-    function createAddButton() {
-        let button = document.createElement("button");
-        let input = document.getElementById("input");
-        button.setAttribute("id", "add_button",
-            "type", "button",
-            "name", "button");
-        button.innerHTML = "Add";
-        document.getElementById("app").appendChild(button);
-        input.addEventListener("keyup", function (event) {
-            if (event.keyCode === 13) {
-                event.preventDefault();
-                button.click();
-                input.value = "";
-            }
-        });
-        document.getElementById("add_button").onclick = function () {
-            if (input.value !== "") {
-                table.push(input.value);
-                console.log(table);
-                input.value = "";
-            }
-        };
-    }
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            addButton.click();
+            input.value = "";
+        }
+    });
 
+    createButton(function () {
+        document.getElementById("result").innerHTML = table.join(", ");
+    }, "showTableButton", "Show Table");
 
-    function createShowTableButton() {
-        let button = document.createElement("button");
-        button.setAttribute("id", "showTableButton",
-            "type", "button",
-            "name", "button",
-            "onclick", "getValue()");
-        button.innerHTML = "Show Table";
-        document.getElementById("app").appendChild(button);
-        //document.getElementById("result").textContent = table.join(", ");
-        //document.getElementById("result").innerHTML = table.join(", ");
-        document.getElementById("showTableButton").onclick = function () {
-            document.getElementById("result").innerHTML = table.join(", ");
-        };
-    }
-
-    // function createClearButton() {
-    //     let button = document.createElement("button");
-    //     button.setAttribute("id", "clearButton",
-    //         "type", "button",
-    //         "name", "button",
-    //         "onclick", "getValue()");
-    //     button.innerHTML = "Clear Table";
-    //     document.getElementById("app").appendChild(button);
-    //     document.getElementById("clearButton").onclick = function () {
-    //         table = [];
-    //     };
-    //}
-
-
-    createInputField();
-    createAddButton();
-    createShowTableButton();
-    //createClearButton();
     createButton(function () {
         table = [];
         document.getElementById("result").innerHTML = "";
